7e83245cc99a6ba5335bb167d24c2752e44ac7f3
Vitalii Onanko <v.onanko@8ways.ch>
2018-10-29 13:16:05 +0800

    Change unzip.sh

--- unzip.sh
+++ unzip.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 DIR=/var/www/expcheck
+UPLOADS_DIR=$DIR/storage/app/uploads
 
-for file in $DIR/pool.zip
+for file in $UPLOADS_DIR/pool.zip
 do
     POOL_DIR_NAME="`date +%m`_`date +%d`_`date +%y`-pool"
-    unzip -n "$file" -d $DIR/tmp &&
-    mkdir $DIR/$POOL_DIR_NAME &&
-    mv $DIR/tmp/* $DIR/$POOL_DIR_NAME/list.txt
-    rm -irf $DIR/tmp
+    unzip -n "$file" -d $UPLOADS_DIR/tmp &&
+    mkdir $UPLOADS_DIR/$POOL_DIR_NAME &&
+    mv $UPLOADS_DIR/tmp/* $UPLOADS_DIR/$POOL_DIR_NAME/list.txt
+    rm -irf $UPLOADS_DIR/tmp
 done

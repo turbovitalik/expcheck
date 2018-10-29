ad3af3ac91e39aea44bc5047a4a3a0f5b0f94e66
Vitalii Onanko <v.onanko@8ways.ch>
2018-10-29 16:30:10 +0800

    Add some migrations and change in unzip.sh

--- unzip.sh
+++ unzip.sh
@@ -6,7 +6,7 @@ POOLS_UNZIP_DIR=$DIR/storage/app/pool_downloads
 
 for file in $UPLOADS_DIR/pool.zip
 do
-    POOL_DIR_NAME="`date +%m`_`date +%d`_`date +%y`-pool"
+    POOL_DIR_NAME="`date +%m`_`date +%d`_`date +%Y`-pool"
     unzip -n "$file" -d $UPLOADS_DIR/tmp &&
     mkdir $POOLS_UNZIP_DIR/$POOL_DIR_NAME &&
     mv $UPLOADS_DIR/tmp/* $POOLS_UNZIP_DIR/$POOL_DIR_NAME/list.txt
